d57c304c78449e9f44427395a14782ca8887d851
Ravi Bhure <ravibhure@gmail.com>
2017-08-28 14:07:06 +0530

    fix ansible to setup multiple nodes

--- install/delete-infra-with-ansible.sh
+++ install/delete-infra-with-ansible.sh
@@ -7,8 +7,11 @@ source env.sh
 # Run ansible
 cd ${PWD}/..
 
-echo -n "Enter InstanceID and press [ENTER]: "
-read instanceid
+echo -n "Enter wordpress InstanceID and press [ENTER]: "
+read wp_instanceid
+
+echo -n "Enter monitoring InstanceID and press [ENTER]: "
+read mon_instanceid
 
 ansible-playbook terminate.yml \
 -e "aws_env=${AWS_ENV}" \
@@ -17,4 +20,5 @@ ansible-playbook terminate.yml \
 -e "aws_region=${CAKE_AWS_REGION}" \
 -e "aws_key_name=${CAKE_KEY_NAME}" \
 -e "aws_key_path=${CAKE_KEY_PATH}" \
--e "instance_ids=${instanceid}"
+-e "wp_instance_ids=${wp_instanceid}" \
+-e "mon_instance_ids=${mon_instanceid}"
